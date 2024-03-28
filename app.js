5a1948ef778d0ce2ee4659de3e6c22311ee3953b
Santiago Cano <sanblackx1@gmail.com>
2024-03-28 03:50:11 -0400

    ADD: Store products in JSON format (Fake DB)

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import path from "node:path";
-import express from "express";
-import bps from "body-parser";
+import path from 'node:path';
+import express from 'express';
+import bps from 'body-parser';
 
-import display from "./routes/display.js";
-import admin from "./routes/admin.js";
-import { error404 } from "./controllers/On404.js";
+import display from './routes/shop.js';
+import admin from './routes/admin.js';
+import { error404 } from './controllers/On404.js';
 
 const app = express();
 const PORT = 3000;
 
-app.set("x-powered-by", false);
+app.set('x-powered-by', false);
 
 // Adding a global configuration value | Ref: https://expressjs.com/en/5x/api.html#app.set
-app.set("view engine", "ejs");
-app.use(express.static(path.join(process.cwd(), "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use(bps.urlencoded({ extended: false }));
 
 app.use(display);
-app.use("/admin", admin);
+app.use('/admin', admin);
 
 app.use(error404);
 
