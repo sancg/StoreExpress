70ab5229d3349c2987e742ef3c91a357fa3da4b5
Santiago Cano <sanblackx1@gmail.com>
2024-04-03 02:13:27 -0400

    NEW: Product Model fetchProduct() can accept callback or promise

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -3,6 +3,6 @@ import { getProductDetail, getStore } from '../controllers/store';
 const store = Router();
 
 store.get('/', getStore);
-store.get('/shop/detail/:productId', getProductDetail);
+store.get('/shop/detail/:productId', async (req, res) => await getProductDetail(req, res));
 
 export default store;
