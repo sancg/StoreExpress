43de3597fdc1b80c7a94a3d1002fc975ad5ef5ea
topduh <sanblackx1@gmail.com>
2025-07-03 07:07:30 -0500

    CHORE: Code review

--- src/controllers/store.ts
+++ src/controllers/store.ts
@@ -10,10 +10,9 @@ export const getStore = (_: Request, res: Response) => {
 
 export const getProductDetail = async (req: Request, res: Response) => {
   const { productId } = req.params;
-  console.log({ productId });
+  // console.log({ productId });
   const prod = await Product.fetchProduct(productId);
   // console.log(prod);
-  // TODO: Create the logic to retrieve the product info based on its ID. Apply frontend
   res.render('shop/detailProduct', {
     prod,
     path: `/shop/${productId}`,
